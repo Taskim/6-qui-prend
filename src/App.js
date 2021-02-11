a1c3294dcd430ba5a20738ff231e43f94d7b1732
Taskim <tony.miksa@getalma.eu>
2021-02-11 21:00:03 +0100

    feat: add card picker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import cards from "./cards.webp";
 
 function App() {
+  const [cardNumber, setCardNumber] = useState(54);
+  const [playedCards, setPlayedCards] = useState([]);
+
+  const getCardPosition = (index) => {
+    const x = (index % 10) * 11.1;
+    const y = Math.floor(index / 10) * 10;
+    return `${x}% ${y}%`;
+  };
+
+  console.log("playedCards: ", playedCards);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        onChange={(e) => setCardNumber(parseInt(e.target.value))}
+        value={cardNumber}
+      />
+      <div className="App">
+        {Array.from(Array(cardNumber).keys()).map((x, i) => (
+          <div
+            key={i}
+            style={{
+              height: 103,
+              width: 69,
+              backgroundImage: `url(${cards})`,
+              backgroundPosition: getCardPosition(i),
+              border: "1px solid #333",
+              margin: 5,
+              position: "relative",
+              cursor: "pointer",
+            }}
+            onClick={() => setPlayedCards([...playedCards, i])}
+          >
+            <div
+              style={{
+                backgroundColor: playedCards.includes(i)
+                  ? "rgba(0,0,0, 0.5)"
+                  : "",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                transition: "all 0.3s ease",
+              }}
+            />
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => setPlayedCards([])}
+        style={{
+          backgroundColor: "#FE5B5F",
+          border: 0,
+          borderRadius: 3,
+          padding: "12px 16px",
+          color: "#fff",
+          marginTop: 140,
+        }}
+      >
+        RESET
+      </button>
+    </>
   );
 }
 
